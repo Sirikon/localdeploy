b445cc06509602c67e122b8170ef32f26f09313f
Carlos Fernández <sirikon2@gmail.com>
2016-08-22 21:40:59 +0200

    Full project creation

--- project.go
+++ project.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"io"
 	"github.com/urfave/cli"
+	"os/user"
 )
 
 const Workspace = "/srv/molly"
@@ -75,6 +76,14 @@ func (p *Project) RunDeploymentScript() error {
 	return nil
 }
 
+func (p *Project) CreateDeploymentScript() error {
+	return ioutil.WriteFile(Workspace + "/" + p.Name + "/deploy.sh", []byte("unzip $MOLLY_ARTIFACT\n"), 0700)
+}
+
+func (p *Project) CreateRunScript() error {
+	return ioutil.WriteFile(Workspace + "/" + p.Name + "/run.sh", []byte("# Write here the run command\n"), 0700)
+}
+
 func (p *Project) StoreArtifact(fileReader io.Reader) error {
 	var artifactFile *os.File
 
@@ -104,7 +113,7 @@ func (p *Project) SaveConfig() error {
 		projectFileBytes = out
 	}
 
-	if err := ioutil.WriteFile(projectFilePath, projectFileBytes, 0777); err != nil {
+	if err := ioutil.WriteFile(projectFilePath, projectFileBytes, 0600); err != nil {
 		return err
 	}
 
@@ -133,6 +142,25 @@ func (p *Project) LoadConfig() error {
 	return nil
 }
 
+func (p *Project) CreateService() error {
+	currentUser, _ := user.Current()
+
+	return ioutil.WriteFile("/etc/systemd/system/molly-" + p.Name + ".service", []byte(`[Service]
+WorkingDirectory=` + (Workspace + "/" + p.Name + "/files") + `
+ExecStart=/bin/sh ` + (Workspace + "/" + p.Name + "/run.sh") + `
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=` + ("molly-" + p.Name) + `
+User=` + (currentUser.Username) + `
+Group=` + (currentUser.Username) + `
+LimitNOFILE=64000
+
+[Install]
+WantedBy=multi-user.target
+`), 0644)
+}
+
 func (p *Project) RestartService() error {
 	cmd := exec.Command("/usr/sbin/service", p.Config.Service, "restart")
 	out, err := cmd.CombinedOutput()
@@ -152,10 +180,21 @@ func AddProjectAction(c *cli.Context) error {
 		Config: ProjectConfig{
 			Name: projectName,
 			Token: "lalala",
-			Service: projectName,
+			Service: "molly-" + projectName,
 		},
 	}
 	if err := project.CreateFilesFolder(); err != nil {
+		return err
+	}
+	if err := project.CreateDeploymentScript(); err != nil {
+		return err
+	}
+	if err := project.CreateRunScript(); err != nil {
+		return err
+	}
+	if err := project.CreateService(); err != nil {
+		fmt.Println("Error while creating the service")
+		fmt.Println(err)
 		return err
 	}
 	if err := project.SaveConfig(); err != nil {
