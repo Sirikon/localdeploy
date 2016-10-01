f4969e5d61ff5071a47d736ebada4b92b973a6db
Carlos Fernández <sirikon2@gmail.com>
2016-10-01 17:14:06 +0200

    Extra info for errors

--- service_strategy_systemd.go
+++ service_strategy_systemd.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os/exec"
 	"errors"
+	"fmt"
 )
 
 type SystemdServiceStrategy struct {
@@ -30,28 +31,33 @@ WantedBy=multi-user.target
 }
 
 func (p SystemdServiceStrategy) Start(service Service) error {
+	commandToExecute := "Command to execute: /usr/sbin/service " + service.Name + " start"
+	fmt.Println("Command to execute: /usr/sbin/service " + service.Name + " start")
 	cmd := exec.Command("/usr/sbin/service", service.Name, "start")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return errors.New("There was an error while trying to restart the service: " + err.Error() + "\n" + string(out))
+		return errors.New("There was an error while trying to restart the service: " + err.Error() + "\n" + string(out) + "\n\nUsed command:\n" + commandToExecute)
 	}
 	return nil
 }
 
 func (p SystemdServiceStrategy) Stop(service Service) error {
+	commandToExecute := "Command to execute: /usr/sbin/service " + service.Name + " stop"
+	fmt.Println("Command to execute: /usr/sbin/service " + service.Name + " stop")
 	cmd := exec.Command("/usr/sbin/service", service.Name, "stop")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return errors.New("There was an error while trying to restart the service: " + err.Error() + "\n" + string(out))
+		return errors.New("There was an error while trying to restart the service: " + err.Error() + "\n" + string(out) + "\n\nUsed command:\n" + commandToExecute)
 	}
 	return nil
 }
 
 func (p SystemdServiceStrategy) Restart(service Service) error {
+	commandToExecute := "Command to execute: /usr/sbin/service " + service.Name + " restart"
 	cmd := exec.Command("/usr/sbin/service", service.Name, "restart")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return errors.New("There was an error while trying to restart the service: " + err.Error() + "\n" + string(out))
+		return errors.New("There was an error while trying to restart the service: " + err.Error() + "\n" + string(out) + "\n\nUsed command:\n" + commandToExecute)
 	}
 	return nil
 }
