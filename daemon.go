0b8404b0282f49cb48831cfe3949dd70a9b5125a
Carlos Fernández <sirikon2@gmail.com>
2017-03-14 00:21:50 +0100

    Added daemon updated file

--- daemon.go
+++ daemon.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"mime/multipart"
 
+	"bytes"
+
 	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli"
 )
@@ -30,13 +32,25 @@ func (da DaemonAction) Run(c *cli.Context) error {
 
 		project := Project{}
 		var uploadedFile multipart.File
+		var deploymentLog bytes.Buffer
+		projectName := req.PostForm("project")
+		receivedToken := req.PostForm("token")
+
+		deploymentLog.WriteString("=== Starting Deployment ===\n")
 
 		p := Promise{}
 		p.Then(func() error {
-			return da.projectLogic.GetByName(req.PostForm("project"), &project)
+			deploymentLog.WriteString("- Getting project with name '" + projectName + "'\n")
+			if projectName == "" {
+				return errors.New("Project name can't be empty")
+			}
+			if err := da.projectLogic.GetByName(projectName, &project); err != nil {
+				return errors.New("Project '" + projectName + "' doesn't exist")
+			}
+			return da.projectLogic.GetByName(projectName, &project)
 		})
 		p.Then(func() error {
-			var receivedToken = req.PostForm("token")
+			deploymentLog.WriteString("- Validating received token\n")
 			var tokenIsCorrect = da.projectLogic.CheckToken(project, receivedToken)
 			if !tokenIsCorrect {
 				return errors.New("Wrong token")
@@ -44,6 +58,7 @@ func (da DaemonAction) Run(c *cli.Context) error {
 			return nil
 		})
 		p.Then(func() error {
+			deploymentLog.WriteString("- Reading received artifact\n")
 			file, _, err := req.Request.FormFile("artifact")
 			if err != nil {
 				return errors.New("Error while reading the uploaded file")
@@ -52,20 +67,26 @@ func (da DaemonAction) Run(c *cli.Context) error {
 			return nil
 		})
 		p.Then(func() error {
+			deploymentLog.WriteString("- Storing received artifact\n")
 			return da.projectLogic.StoreArtifact(project, uploadedFile)
 		})
 		p.Then(func() error {
+			deploymentLog.WriteString("- Running deployment script\n")
 			return da.projectLogic.RunDeploymentScript(project)
 		})
 		p.Then(func() error {
+			deploymentLog.WriteString("- Restarting the service\n")
 			return da.projectLogic.RestartService(project)
 		})
 		p.Then(func() error {
-			req.String(200, "Done\n")
+			deploymentLog.WriteString("Done!\n")
+			req.String(200, deploymentLog.String())
 			return nil
 		})
 		p.Catch(func(err error) {
-			req.String(400, err.Error()+"\n")
+			deploymentLog.WriteString("There was an ERROR:\n\n")
+			deploymentLog.WriteString(err.Error() + "\n")
+			req.String(400, deploymentLog.String())
 		})
 		p.Run()
 
